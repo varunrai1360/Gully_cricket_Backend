064f7e129b8e2104d9331690b0d1465a9232a929
manish <mrai8450@gmail.com>
2024-08-18 16:02:51 +0530

    changes for deployment

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ const PORT = process.env.PORT || 2040;
 //     console.log('Listening to port:  ' + 2040);
 // });
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'client','build','index.html'))
-    });
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.join(__dirname,'client','build','index.html'))
+//     });
+// }
 app.listen(PORT, function() {
     console.log('Listening to port:  ' + PORT);
 });
